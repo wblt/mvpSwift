8c5cf8f4eefa3b3f8c7a837a1fec2fe9f3934c61
haha <haha@lizzydeiMac-3.local>
2017-06-05 11:25:02 +0800

    wb

--- Test/AppDelegate.swift
+++ Test/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         let rootVC:RootController = RootController();
         //lengting
+        // wangbing
         window = UIWindow(frame:UIScreen.main.bounds);
         window?.rootViewController = rootVC;
         window?.makeKeyAndVisible()
